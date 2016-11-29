ec741b6c8e9b2332ccd493328d78d31259c5f778
irkush <irkush@gmail.com>
2016-11-29 09:54:58 +0100

    CheckAll

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,11 +4,25 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Version 1.1";
+		public static readonly string VERSION = "Version 1.1 CheckAll";
 
 		public static int BetRequest(JObject gameState)
 		{
-		    return 10;
+
+
+		    var current_buy_in = gameState["current_buy_in"].Value<int>();
+
+
+		    var in_action = gameState["in_action"].Value<int>();
+
+            var our_current_bet = gameState["players"][in_action]["bet"].Value<int>();
+
+
+            // Allways check
+
+            return current_buy_in - our_current_bet;
+
+
 
 
 			//TODO: Use this method to return the value You want to bet
