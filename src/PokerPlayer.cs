a224655cbb12b4cfd1a40e00d207575649e20ad2
irkush <irkush@gmail.com>
2016-11-29 09:37:14 +0100

    First commit

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,12 +4,15 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "Version 1";
 
 		public static int BetRequest(JObject gameState)
 		{
+		    return 100;
+
+
 			//TODO: Use this method to return the value You want to bet
-			return 0;
+			//return 0;
 		}
 
 		public static void ShowDown(JObject gameState)
