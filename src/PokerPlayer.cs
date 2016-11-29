b8aa1a865e8319a69d2426f2cf6d38ef1818c906
irkush <irkush@gmail.com>
2016-11-29 11:03:20 +0100

    Fixed issue with early folding

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -6,16 +6,34 @@ using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
-	public static class PokerPlayer
-	{
-		public static readonly string VERSION = "Version 1.1 Bet 1000 on pre-flop pair";
+    public static class PokerPlayer
+    {
+        public static readonly string VERSION = "Version 1.1 Bet on other team losing on pre-flop pair";
 
-		public static int BetRequest(JObject gameState)
-		{
+        public static Dictionary<string,int> Dictionary = new Dictionary<string, int>()
+                {
+                    {"1",1 },
+                    {"2",2 },
+                    {"3",3 },
+                    {"4",4 },
+                    {"5",5 },
+                    {"6",6 },
+                    {"7",7 },
+                    {"8",8 },
+                    {"9",9 },
+                    {"10",10 },
+                    {"J",11 },
+                    {"Q",12 },
+                    { "K",13 },
+                    {"A",14 },
+                };
+
+        public static int BetRequest(JObject gameState)
+        {
             Console.Error.WriteLine("Betting round started");
-		    int value = 0;
-		    try
-		    {
+            int value = 0;
+            try
+            {
                 //return 30;
 
                 var current_buy_in = gameState["current_buy_in"].Value<int>();
@@ -23,72 +41,98 @@ namespace Nancy.Simple
 
                 var in_action = gameState["in_action"].Value<int>();
 
-                var our_current_bet = gameState["players"][in_action]["bet"].Value<int>();
+
+                var ourPlayer = gameState["players"][in_action];
 
 
-		        var hole_cards = gameState["players"][in_action]["hole_cards"];
+                var our_current_bet = ourPlayer["bet"].Value<int>();
 
 
-		        var cards = new List<Tuple<string, string>>();
-		        foreach (var card in hole_cards)
-		        {
-		            var rank = card["rank"].Value<string>();
-		            var suit = card["suit"].Value<string>();
-
-                    cards.Add(new Tuple<string, string>(rank,suit));
-
-		        }
-		        if (cards.Count == 2)
-		        {
-		            Console.Error.WriteLine("We have " + cards[0].Item1 + " " + cards[1].Item1);
-		        }
-
-
-		        var community_cards = gameState["community_cards"];
-		        if (!community_cards.HasValues)
-		        {
-		            Console.Error.WriteLine("No community cards in place");
-		            if (cards.Count == 2 && cards[0].Item1 == cards[1].Item1)
-		            {
-                        Console.Error.WriteLine("We have pairs, going all in");
-		                value = 1000;
-		            }
-
-
-		        }
-		        else
-		        {
-                    value = current_buy_in - our_current_bet;
-                }
-
-
-                // foreach()
-
-
-                // Om vi har bra par gå all in. ( KnKn, DD, KK, EE )
-
-
-                // Allways check
+                var hole_cards = ourPlayer["hole_cards"];
 
                
 
 
+
+                var cards = new List<Tuple<string, string>>();
+                foreach (var card in hole_cards)
+                {
+                    var rank = card["rank"].Value<string>();
+                    var suit = card["suit"].Value<string>();
+
+                    cards.Add(new Tuple<string, string>(rank, suit));
+
+                }
+                if (cards.Count == 2)
+                {
+                    Console.Error.WriteLine("We have " + cards[0].Item1 + " " + cards[1].Item1);
+                }
+
+                var community_cards = gameState["community_cards"];
+
+                int betValue;
+                bool hasOtherPlay = GetPlay(out betValue,cards, community_cards, ourPlayer);
+                if (!hasOtherPlay)
+                {
+                    return current_buy_in - our_current_bet;
+                }
+
+
+
             }
             catch (Exception ex)
-		    {
-		        Console.Error.Write(ex);
-		    }
+            {
+                Console.Error.Write(ex);
+            }
 
             Console.Error.WriteLine("Playing: " + value);
 
-			//TODO: Use this method to return the value You want to bet
-			return value;
-		}
+            //TODO: Use this method to return the value You want to bet
+            return value;
+        }
 
-		public static void ShowDown(JObject gameState)
-		{
-			//TODO: Use this method to showdown
-		}
-	}
+        private static bool GetPlay(out int betValue, List<Tuple<string,string>> cards, JToken communityCards, JToken ourPlayer)
+        {
+
+            if (!communityCards.HasValues)
+            {
+                Console.Error.WriteLine("No community cards in place");
+                if (cards.Count == 2)
+                {
+                    // Same rank
+                    if (cards[0].Item1 == cards[1].Item1)
+                    {
+                        Console.Error.WriteLine("We have pairs, going all in");
+                        betValue = ourPlayer["stack"].Value<int>();
+                        return true;
+                    }
+
+                    // Same suit
+                    if (cards[0].Item2 == cards[1].Item2)
+                    {
+                        Console.Error.WriteLine("We have Colors");
+                        //value = ourPlayer["stack"].Value<int>();
+
+                        var firstCardValue = Dictionary[cards[0].Item1];
+                        var secondCardValue = Dictionary[cards[1].Item1];
+
+                        if (Math.Abs(firstCardValue - secondCardValue) == 1)
+                        {
+                            // Possible straight flush
+                        }
+                    }
+
+                }
+
+            }
+            betValue = 0;
+            return false;
+        }
+
+        public static void ShowDown(JObject gameState)
+        {
+            //TODO: Use this method to showdown
+        }
+    }
 }
 
