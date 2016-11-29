d53acf33c85fe2c3c6c27e30a83b1f84e438b25e
irkush <irkush@gmail.com>
2016-11-29 10:13:46 +0100

    Added logging?

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CanParseJOBject()
+        {
+
+
+
+            var file = File.ReadAllText("data.json");
+
+
+            var gameState = JObject.Parse(file);
+
+
+            var current_buy_in = gameState["current_buy_in"].Value<int>();
+
+
+            var in_action = gameState["in_action"].Value<int>();
+
+            var our_current_bet = gameState["players"][in_action]["bet"].Value<int>();
+
+
+
+
+            Assert.Fail();
+
+
+
+        }
+    }
+}
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,35 +1,42 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Version 1.1 CheckAll";
+		public static readonly string VERSION = "Version 1.1 CheckAll With Logging";
 
 		public static int BetRequest(JObject gameState)
 		{
-		    return 30;
+		    try
+		    {
+                //return 30;
 
-		    var current_buy_in = gameState["current_buy_in"].Value<int>();
-
-
-		    var in_action = gameState["in_action"].Value<int>();
-
-            var our_current_bet = gameState["players"][in_action]["bet"].Value<int>();
+                var current_buy_in = gameState["current_buy_in"].Value<int>();
 
 
-            // Om vi har bra par gå all in. ( KnKn, DD, KK, EE )
+                var in_action = gameState["in_action"].Value<int>();
+
+                var our_current_bet = gameState["players"][in_action]["bet"].Value<int>();
 
 
-            // Allways check
-
-            return current_buy_in - our_current_bet;
+                // Om vi har bra par gå all in. ( KnKn, DD, KK, EE )
 
 
+                // Allways check
 
+                return current_buy_in - our_current_bet;
+
+
+            }
+            catch (Exception ex)
+		    {
+		        Console.Error.Write(ex);
+		    }
 
 			//TODO: Use this method to return the value You want to bet
-			//return 0;
+			return 20;
 		}
 
 		public static void ShowDown(JObject gameState)
